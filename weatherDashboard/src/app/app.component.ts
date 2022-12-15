13dd7ba9130425070af1b7448707abf5383bcb3f
Ucr4y <robinmayrhofer2@gmail.com>
2022-12-15 17:07:14 +0100

    get temperature

--- weatherDashboard/src/app/app.component.ts
+++ weatherDashboard/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { Result, WeatherDataService } from './weather-data.service';
+import {
+  Result,
+  WeatherDataService,
+  WeatherResponse,
+} from './weather-data.service';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +12,19 @@ import { Result, WeatherDataService } from './weather-data.service';
 })
 export class AppComponent {
   public location: string;
-  public geoData?: Result;
+  public weatherData?: WeatherResponse;
 
   constructor(public service: WeatherDataService) {
     this.location = '';
   }
 
   public loadGeoData() {
-    this.service
-      .loadGeoData(this.location)
-      .subscribe((data) => (this.geoData = data.results[0]));
+    this.service.loadGeoData(this.location).subscribe((data) => {
+      if (data.results.length > 0) {
+        this.service
+          .loadTemperature(data.results[0].lon, data.results[0].lat)
+          .subscribe((data) => (this.weatherData = data));
+      }
+    });
   }
 }
--- weatherDashboard/src/app/weather-data.service.ts
+++ weatherDashboard/src/app/weather-data.service.ts
@@ -11,6 +11,20 @@ export interface Results {
   results: Result[];
 }
 
+export interface Weather {
+  icon: string;
+  description: string;
+}
+
+export interface WeatherMain {
+  temp: number;
+}
+
+export interface WeatherResponse {
+  weather: Weather[];
+  main: WeatherMain;
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -20,6 +34,15 @@ export class WeatherDataService {
   public loadGeoData(location: string): Observable<Results> {
     return this.http.get<Results>(
       `https://api.geoapify.com/v1/geocode/search?text=${location}&format=json&apiKey=7a3839bb1c554274a2e3527db778f9ad`
+    );
+  }
+
+  public loadTemperature(
+    lon: number,
+    lat: number
+  ): Observable<WeatherResponse> {
+    return this.http.get<WeatherResponse>(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=de&units=metric&appid=8ff4dc4f6c29f630e4461e41def885bd`
     );
   }
 }
